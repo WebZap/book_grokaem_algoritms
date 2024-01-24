31f71b56489c682c64b19f7ca6f61ba9295ed83e
Zaporozhtsev Pavel <webzap@proton.me>
2024-01-24 22:05:29 +0300

    create algoritm

---
+++ 1.binary_search.py
@@ -0,0 +1,18 @@
+def binary_search(list,item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess_number = list[mid]
+        if guess_number == item:
+            return mid
+        if guess_number > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1,2,3,4,5,6]
+
+print(binary_search(my_list,123))
